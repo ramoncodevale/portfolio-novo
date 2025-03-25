ef0bbe649250b16e4a58fe7676c81f34e67293a0
ramoncodevale <ramonvalentim88@gmail.com>
2025-03-25 12:48:03 -0300

    chore: corrigindo o componente do projeto

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,70 +1,124 @@
-	               import { Smartphone, Mail, Github, Linkedin } from "lucide-react";
+import { Smartphone, Mail, Github, Linkedin } from "lucide-react";
 
-				import Link from "next/link"
+import Link from "next/link";
 
-				export function Footer() {
-				    return (
-				      <footer className="py-[5rem] bg-gradient-to-r from-san-felix-500 to-san-felix-800">
-				        <div className="flex flex-col md:flex-row gap-5 justify-between w-[90%] max-w-[540px] sm:max-w-[720px] py-4 md:max-w-[1280px] m-[auto]">
-				        <h1>Ramon</h1>
-				    
-				     <div>
-				      <p className="text-san-felix-50 font-extrabold">Habilidades</p>
-				      <ul className="flex flex-col gap-3 mt-5">
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">HTML</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">CSS</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">JavaScript</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">TypeScript</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">Tailwind CSS</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">React</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">Next</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">Git</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">Github</li>
-				      </ul>
-				      </div>
-				 
-				    <div>
-				        <p className="text-san-felix-50 font-extrabold">Menu</p>
-				      <ul className="flex flex-col gap-3 mt-5">
-				        <li>
-				          <Link href="/" className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
-				          Inicio
-				          </Link>
-				        </li>
-				        <li>
-				        <Link href="/sobre-mim" className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
-				          Sobre Mim
-				        </Link>
-				        </li>
-				        <li>
-				        <Link href="/habilidades" className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
-				          Habilidades
-				        </Link>
-				        </li>
-				        <li>
-				        <Link href="/projetos" className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
-				          Projetos
-				          </Link>
-				        </li>
-				        <li>
-				        <Link href="/contato" className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
-				          Contato
-				         </Link>
-				        </li>
-				      </ul>
-				      </div>
-				   
-				     <div>
-				      <p className="text-san-felix-50 font-extrabold">Contato</p>
-				      <ul className="flex flex-col gap-3 mt-5">
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px] flex items-center gap-2"><Smartphone /> (11) 98887-2804</li>
-				        <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px] flex items-center gap-2"><Mail /> ramonvalentim88@gmail.com</li>
-				        <li><a className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px] flex items-center gap-2" href="https://www.github.com/ramoncodevale/" target="_blank" rel="nofollow"><Github /> ramoncodevale</a></li>
-				        <li><a className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px] flex items-center gap-2" href="https://www.linkedin.com/in/ramonvalentim88/" target="_blank" rel="nofollow"><Linkedin /> ramonvalentim</a></li>
-				      </ul>
-				      </div>
-				      </div>
+export function Footer() {
+  return (
+    <footer className="py-[5rem] bg-gradient-to-r from-san-felix-500 to-san-felix-800">
+      <div className="flex flex-col md:flex-row gap-5 justify-between w-[90%] max-w-[540px] sm:max-w-[720px] py-4 md:max-w-[1280px] m-[auto]">
+        <h1>Ramon</h1>
 
-				        </footer>
-				    )
-				}
+        <div>
+          <p className="text-san-felix-50 font-extrabold">Habilidades</p>
+          <ul className="flex flex-col gap-3 mt-5">
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              HTML
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              CSS
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              JavaScript
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              TypeScript
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              Tailwind CSS
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              React
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              Next
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              Git
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]">
+              Github
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-san-felix-50 font-extrabold">Menu</p>
+          <ul className="flex flex-col gap-3 mt-5">
+            <li>
+              <Link
+                href="/"
+                className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]"
+              >
+                Inicio
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/sobre-mim"
+                className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]"
+              >
+                Sobre Mim
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/habilidades"
+                className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]"
+              >
+                Habilidades
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/projetos"
+                className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]"
+              >
+                Projetos
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/contato"
+                className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px]"
+              >
+                Contato
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-san-felix-50 font-extrabold">Contato</p>
+          <ul className="flex flex-col gap-3 mt-5">
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px] flex items-center gap-2">
+              <Smartphone /> (11) 98887-2804
+            </li>
+            <li className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px] flex items-center gap-2">
+              <Mail /> ramonvalentim88@gmail.com
+            </li>
+            <li>
+              <a
+                className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px] flex items-center gap-2"
+                href="https://www.github.com/ramoncodevale/"
+                target="_blank"
+                rel="nofollow"
+              >
+                <Github /> ramoncodevale
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-san-felix-300 hover:text-san-felix-200 cursor-pointer hover:underline text-sm leading-[18px] flex items-center gap-2"
+                href="https://www.linkedin.com/in/ramonvalentim88/"
+                target="_blank"
+                rel="nofollow"
+              >
+                <Linkedin /> ramonvalentim
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,39 +1,60 @@
-	import { Breadcrumb } from "../_components/breadcrumb"
-			import { Button } from "../_components/button"
-			import Image from "next/image";
-			import { PageTitle } from "../_components/page-title"
-			import { Projetos } from "../_constants/index"
+import { Breadcrumb } from "../_components/breadcrumb";
+import { Button } from "../_components/button";
+import Image from "next/image";
+import { PageTitle } from "../_components/page-title";
+import { Projetos } from "../_constants/index";
 
+export default function ProjetosPage() {
+  const uniqueProjects = [...new Map(Projetos.map((p) => [p.id, p])).values()];
 
+  return (
+    <>
+      <Breadcrumb pageTitle="Projetos" />
+      <PageTitle title="Projetos" />
 
-			export default function ProjetosPage(){
-			    return (
-			        <>
-			        <Breadcrumb pageTitle="Projetos" />
-			        <PageTitle title="Projetos" />
-
-			        <section className="py-[5rem] bg-san-felix-50">
-			        <div className="w-[90%] max-w-[540px] sm:max-w-[720px] md:max-w-[1280px] m-[auto]">
-			        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-			        
-			          {Projetos.map((projects) => (
-			            <div key={projects.id} className="w-full border bg-white border-san-felix-950-[0.2px]">
-			              <Image src={projects.image} alt={projects.name} width={210} height={210} title={projects.name} className="w-full h-[210px]" />
-			              <div className="p-6">
-			                <p className="text-xl font-bold leading-[33.6px]">{projects.name}</p>
-			                <p className="text-[#5b5b5b] leading-[24px] line-clamp-3">{projects.description}</p>
-			                <div className="flex gap-3 pt-6">
-			                <Button href={projects.link1} title={`Veja o projeto online - ${projects.name}`} className="w-full text-center py-1.5 bg-san-felix-950 text-white font-semibold  hover:bg-san-felix-600" name="Projeto"  />
-			                <Button href={projects.link2} title={`Veja o repositório no Github - ${projects.name}`} className="w-full text-center py-1.5 bg-san-felix-950 text-white font-semibold hover:bg-san-felix-600" name="Repositório"  />
-			                </div>
-			              </div>
-			            </div>
-			          ))}
-			          </div>
-			        </div>
-
-			 
-			        </section>
-			        </>
-			    )
-			}
+      <section className="py-[5rem] bg-san-felix-50">
+        <div className="w-[90%] max-w-[540px] sm:max-w-[720px] md:max-w-[1280px] m-[auto]">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {uniqueProjects.map((project) => (
+              <div
+                key={project.id}
+                className="w-full border bg-white border-san-felix-950-[0.2px]"
+              >
+                <Image
+                  src={project.image}
+                  alt={project.name}
+                  width={210}
+                  height={210}
+                  title={project.name}
+                  className="w-full h-[210px]"
+                />
+                <div className="p-6">
+                  <p className="text-xl font-bold leading-[33.6px]">
+                    {project.name}
+                  </p>
+                  <p className="text-[#5b5b5b] leading-[24px] line-clamp-3">
+                    {project.description}
+                  </p>
+                  <div className="flex gap-3 pt-6">
+                    <Button
+                      href={project.link1}
+                      title={`Veja o projeto online - ${project.name}`}
+                      className="w-full text-center py-1.5 bg-san-felix-950 text-white font-semibold hover:bg-san-felix-600"
+                      name="Projeto"
+                    />
+                    <Button
+                      href={project.link2}
+                      title={`Veja o repositório no Github - ${project.name}`}
+                      className="w-full text-center py-1.5 bg-san-felix-950 text-white font-semibold hover:bg-san-felix-600"
+                      name="Repositório"
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
