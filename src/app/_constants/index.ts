41b6da7c98aec9ee3322cde0713816655c158d9d
ramoncodevale <ramonvalentim88@gmail.com>
2025-03-29 17:17:57 -0300

    chore: trocando imagem do projeto

--- src/app/_constants/index.ts
+++ src/app/_constants/index.ts
@@ -85,6 +85,6 @@ export const Projetos = [
     link1: "https://interplanetario-delivery.vercel.app/",
     link2: "https://github.com/ramoncodevale/interplanet-rio-delivery",
     image:
-      "https://images.unsplash.com/photo-1589225529399-8705282f98e2?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      "https://images.pexels.com/photos/41953/earth-blue-planet-globe-planet-41953.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
 ];
